c4e005f221dce264780258f229e7534b56220741
ShyamL1319 <sshyamlalchauhan@gmail.com>
2023-03-06 12:34:37 +0530

    delete functionality updated

--- src/app/posts/postlist/postlist.component.ts
+++ src/app/posts/postlist/postlist.component.ts
@@ -3,6 +3,7 @@ import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Post } from 'src/app/models/post.model';
 import { AppState } from 'src/app/store/app.state';
+import { deletePost } from '../store/posts.actions';
 import { getPosts } from '../store/posts.selector';
 
 @Component({
@@ -16,6 +17,11 @@ export class PostlistComponent implements OnInit {
 
   ngOnInit(): void {
     this.posts$ = this.store.select(getPosts);
+  }
+
+  onDeletePost(postId) {
+    if (!confirm('Are you sure to delete the post?')) return;
+    this.store.dispatch(deletePost({id:postId}))
   }
 
 }
--- src/app/posts/store/posts.reducer.ts
+++ src/app/posts/store/posts.reducer.ts
@@ -1,30 +1,28 @@
 import { createReducer, on } from "@ngrx/store";
-import { addPost, updatePost } from "./posts.actions";
+import { addPost, deletePost, updatePost } from "./posts.actions";
 import { initialState } from "./posts.state";
 
 const _postReducer = createReducer(initialState,
-    on(addPost, (state, action) => { 
+    on(addPost, (state, action) => {
         let post = { ...action.post };
-        post.id = (state.posts.length+1).toString()
-        return {
-            ...state,
-            posts:[...state.posts, post]
-        }
+        post.id = (state.posts.length + 1).toString()
+        return { ...state, posts: [...state.posts, post] }
     }),
-    on(updatePost, (state, action) => { 
+    on(updatePost, (state, action) => {
         let updated_post = { ...action.post };
         const posts = state.posts.map(post => {
             if (post.id === updated_post.id) {
                 return { ...updated_post };
-            } else { 
+            } else {
                 return post;
-            } 
+            }
         });
-        return {
-            ...state,
-            posts:posts
-        }
-    })
+        return { ...state, posts: posts }
+    }),
+    on(deletePost, (state, action) => {
+        let afterDeletingPosts = state.posts.filter(post => post.id !== action.id)
+        return { ...state, posts:[...afterDeletingPosts] }
+    }),
 )
 
 
