e0685cc94778688a897c930f21e80758a9d52736
ShyamL1319 <sshyamlalchauhan@gmail.com>
2023-03-09 14:55:24 +0530

    view data using ngrx data

--- src/app/posts/signgle-post/signgle-post.component.ts
+++ src/app/posts/signgle-post/signgle-post.component.ts
@@ -1,7 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { Post } from 'src/app/models/post.model';
+import { PostsService } from 'src/app/services/posts.service';
 import { AppState } from 'src/app/store/app.state';
 import { getPostById } from '../store/posts.selector';
 
@@ -11,13 +13,19 @@ import { getPostById } from '../store/posts.selector';
   styleUrls: ['./signgle-post.component.scss']
 })
 export class SignglePostComponent implements OnInit {
-  post$: Observable<Post>;
+  post: Post;
   constructor(
-    private store:Store<AppState>
+    private store: Store<AppState>,
+    private postService: PostsService,
+    private route : ActivatedRoute,
   ) { }
 
   ngOnInit(): void {
-    this.post$ = this.store.select(getPostById);
+    //this.post$ = this.store.select(getPostById);
+    const id = this.route.snapshot.params['id'];
+    this.postService.entities$.subscribe(posts => {
+      this.post = posts.find(post => post.id === id);
+    });
   }
 
 }
